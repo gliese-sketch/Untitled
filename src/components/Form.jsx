ef966eeedea7e9da2d221c11db9abadd979081fa
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-11 08:04:00 +0000

    add submit button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,4 @@
+import { TbFlareFilled } from "react-icons/tb";
 import Intro from "@/components/Intro";
 
 function Form() {
@@ -39,20 +40,28 @@ function Form() {
           className="h-24 border-b border-stone-700 bg-zinc-50 p-2 placeholder-slate-700 md:bg-lime-400"
           required
         />
-
         <p className="my-5 text-zinc-800">How can we help?</p>
-
         {/* Checkbox */}
-        <section className="grid grid-cols-2 gap-1 md:max-w-96">
+        <section className="mb-12 grid grid-cols-2 gap-1 md:max-w-96">
           {services.map((service, idx) => {
             return (
-              <label key={service + idx} className="flex items-center gap-1">
+              <label
+                key={service + idx}
+                className="flex cursor-pointer items-center gap-1"
+              >
                 <input type="checkbox" name={service} className="size-6" />
                 {service}
               </label>
             );
           })}
         </section>
+        <button
+          type="submit"
+          className="flex justify-center gap-2 rounded-lg bg-stone-950 p-2 text-white"
+        >
+          Let's get started{" "}
+          <TbFlareFilled size={20} className="text-lime-500" />
+        </button>
       </form>
     </>
   );
